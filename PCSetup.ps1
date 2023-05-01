3fca23573c451bc56a75158bcc1413476ea293d8
Simon Thurston <simongethurston@gmail.com>
2023-05-01 13:56:35 -0400

    Added verbosity

--- PCSetup.ps1
+++ PCSetup.ps1
@@ -1,73 +1,97 @@
 $AdminPath = "C:\Admin\"
 $WingetUrl = "https://github.com/microsoft/winget-cli/releases/"
+$XamlUrl = "https://www.nuget.org/api/v2/package/Microsoft.UI.Xaml/2.7.3"
 
 # Check for Admin Rights
+Write-Host "Checking for Administrative Privileges..."
 $identity = [Security.Principal.WindowsIdentity]::GetCurrent()
 $principal = New-Object Security.Principal.WindowsPrincipal $identity
 If (-Not $principal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
-    Write-Host "ERROR: Not running in elevated PowerShell"
-    Write-Host -NoNewline 'Press any key to exit...';
-    $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
+    Write-Host "ERROR: Not running in elevated PowerShell" -ForegroundColor White -BackgroundColor Red
+    Write-Host -NoNewline 'Press any key to exit...' -ForegroundColor White -BackgroundColor Red
+    $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown')
     exit
 }
+Write-Host "Powershell is running with Administrative Privileges."
 
 # Create Adminpath if does not exist
+Write-Host "Checking for Admin folder..."
 If (!(Test-Path $AdminPath)) {
-    New-Item $AdminPath -ItemType Directory -Confirm
+    Write-Host "Admin folder not found. Creating."
+    New-Item $AdminPath -ItemType Directory -Force
+} Else {
+    Write-Host "Admin folder found."
 }
 
 # Registers Powershell Gallery
+Write-Host "Checking for PSGallery Registration..."
 If ($null -eq (Get-PSRepository -Name "PSGallery")) {
+    Write-Host "PSGallery is not registerred. Registering..."
     If (((Get-Host).Version).Major -gt 5) {
         Register-PSRepository -Default -InstallationPolicy Trusted
     } Else {
         Register-PSRepository -Name PSGallery -SourceLocation https://www.powershellgallery.com/api/v2/ -InstallationPolicy Trusted
     }
+} ElseIf ((Get-PSRepository -Name "PSGallery").InstallationPolicy -ne "Trusted") {
+    Write-Host "PSGallery is not trusted. Trusting..."
+    Set-PSRepository -Name "PSGallery" -InstallationPolicy "Trusted"
 }
 
 # Get Manufacturer
 $manufacturer = (Get-CimInstance -ClassName Win32_ComputerSystem).Manufacturer.Split(" ")[0]
+Write-Host "Manufacturer is: $manufacturer"
 
 # Checks if is Dell before running Dell-Specific commands
 If ($manufacturer -eq "Dell") {
 
     # Check for Dell BIOS Provider Module, install if does not exist
+    Write-Host "Checking for Dell BIOS Provider..."
     If ($null -eq (Get-Module -ListAvailable -Name DellBIOSProvider)) {
         Install-Module -Name DellBIOSProvider -AcceptLicense 
     }
-
+    Write-Host "Importing Dell BIOS Provider..."
     Import-Module DellBIOSProvider
 
     # Configure BIOS Power Settings
     $p = "DellSmbios:\PowerManagement\"
     $a = "AcPwrRcvry"
     $v = "Last"
+    Write-Host "Setting BIOS Setting $a as $v"
     If ((Get-ChildItem $p).Attribute.Contains($a) -and !(Get-ChildItem ($p + $a)).CurrentValue.Equals($v)) {
         Set-Item -Path ($p + $a) $v
     }
     $a = "DeepSleepControl"
     $v = "Disabled"
+    Write-Host "Setting BIOS Setting $a as $v"
     If ((Get-ChildItem $p).Attribute.Contains($a) -and !(Get-ChildItem ($p + $a)).CurrentValue.Equals($v)) {
         Set-Item -Path ($p + $a) $v
     }
     $a = "BlockSleep"
     $v = "Enabled"
+    Write-Host "Setting BIOS Setting $a as $v"
     If ((Get-ChildItem $p).Attribute.Contains($a) -and !(Get-ChildItem ($p + $a)).CurrentValue.Equals($v)) {
         Set-Item -Path ($p + $a) $v
     }
 }
 
 # Set Serial Number as Computer Name
+Write-Host "Renaming PC to Service Tag/Serial Number"
 (Get-WmiObject Win32_ComputerSystem).Rename((wmic bios get serialnumber /format:csv | ConvertFrom-Csv).SerialNumber) | Out-Null
+If((Get-WmiObject Win32_ComputerSystem).Name -eq (wmic bios get serialnumber /format:csv | ConvertFrom-Csv).SerialNumber) {
+    Write-Host "Successfully set new PC Name."
+}
 
 # Set Power Settings
+Write-Host "Adjusting Power Settings..."
 $p = Get-CimInstance -Name root\cimv2\power -Class Win32_PowerPlan -Filter "ElementName = 'High Performance'"
 If ($p) {
+    Write-Host "High Performance Profile is available. Setting..."
     powercfg /setactive ([string]$p.InstanceID).Replace("Microsoft:PowerPlan\{", "").Replace("}", "") 
 }
 powercfg /hibernate off
 
 # Set Timezone
+Write-Host "Setting and Synchronizing Time..."
 Set-TimeZone -Id "Eastern Standard Time"
 W32tm /resync /force
 
@@ -76,6 +100,7 @@ W32tm /resync /force
 # &($AdminPath + "Decrapinator.ps1")
 
 # Install Winget
+Write-Host "Downloading Package Manager..."
 $WingetVersion = [System.Net.WebRequest]::Create($WingetUrl + "latest").GetResponse().ResponseUri.OriginalString.split('/')[-1].Trim('v')
 Invoke-WebRequest -Uri ($WingetUrl + "download/v" + $WingetVersion + "/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle") -OutFile ($AdminPath + "winget.msixbundle")
 Add-AppxPackage ($AdminPath + "winget.msixbundle")
@@ -87,10 +112,11 @@ Add-AppxPackage ($AdminPath + "winget.msixbundle")
 
 # Run Dell Command Update
 If ($manufacturer -eq "Dell") {
+    Write-Host "Running Dell Command Update..."
     & winget install Dell.CommandUpdate.Universal --accept-source-agreements --accept-package-agreements | Out-Null
     Start-Process ($Env:Programfiles + "\Dell\CommandUpdate\dcu-cli") -ArgumentList "/scan -outputLog=$AdminPath`dcuscan.log -updateType=bios,firmware,driver,application,others -updateSeverity=security,critical,recommended,optional -silent" -Wait -NoNewWindow
     Start-Process ($Env:Programfiles + "\Dell\CommandUpdate\dcu-cli") -ArgumentList "/applyUpdates -forceUpdate=enable -outputLog=$AdminPath`dcu.log -updateType=bios,firmware,driver,application,others -updateSeverity=security,critical,recommended,optional -silent" -Wait -NoNewWindow
 }
 
 # Self Explanatory
-Restart-Computer
+# Restart-Computer
